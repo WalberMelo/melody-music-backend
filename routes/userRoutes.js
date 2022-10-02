4eeae888b642f3208926e401d7f364de693fcc3d
Walber Melo <dev4wm@gmail.com>
2022-10-02 20:22:40 +0100

    feat(signin): Implemente user signin mvc

---
+++ routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const userController = require("../controllers/userController");
+const userRouter = express.Router();
+
+userRouter.post("/register", userController.postUser);
+
+module.exports = userRouter;
