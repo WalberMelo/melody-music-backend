e10c6bf7a04617b9db92d7a917af834d90edef8a
Agustín García <agustin.tavoite@gmail.com>
2022-10-07 15:35:49 +0200

    g

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db");
-const userRouter = require("./backend/routes/userRoutes");
-const adminRouter = require("./backend/routes/adminRoutes");
-const port = 3000;
+const userRouter = require("./routes/userRoutes");
+const adminRouter = require("./routes/adminRoutes");
+const port = 9001;
 const app = express();
 
 //Connect to Data Base
