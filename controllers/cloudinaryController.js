be32b9d33fdebc378fced11f467b3e0f372db17e
Walber Melo <dev4wm@gmail.com>
2022-10-22 12:54:29 +0100

    fix(playlist): Add thumbnail playlist

--- controllers/cloudinaryController.js
+++ controllers/cloudinaryController.js
@@ -1,41 +1,44 @@
 const cloudinary = require("../services/cloudinary");
 
+async function cloudinarySongUploader(req, res) {
+  const song = req.file.path;
 
-async function cloudinarySongUploader(req, res){
-
-    const song = req.file.path
-    
-    cloudinary.uploader
-    .upload(song, { resource_type: "video", use_filename: true, unique_filename: true} )
+  cloudinary.uploader
+    .upload(song, {
+      folder: "melody/songs",
+      resource_type: "video",
+      use_filename: true,
+      unique_filename: true,
+    })
     .then((result) => {
-        const track = {
-            url: result.url,
-            duration: result.duration
-        }
+      let track = {
+        url: result.url,
+        duration: result.duration,
+      };
 
-        res.send(track)
-
-
+      res.send(track);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
+async function cloudinaryThumbnailUploader(req, res) {
+  let thumbnail = req.file.path;
 
-
-async function cloudinaryThumbnailUploader(req, res){
-
-    const thumbnail = req.file.path
-
-    cloudinary.uploader
-    .upload(thumbnail, { resource_type: "auto", use_filename: true, unique_filename: true} )
+  cloudinary.uploader
+    .upload(thumbnail, {
+      folder: "melody/thumbnail",
+      resource_type: "auto",
+      use_filename: true,
+      unique_filename: true,
+    })
     .then((result) => {
-      console.log(result);
+      res.send({ image: result.url });
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
-module.exports = {cloudinarySongUploader, cloudinaryThumbnailUploader};
+module.exports = { cloudinarySongUploader, cloudinaryThumbnailUploader };
--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -9,19 +9,46 @@ playlistRouter.post(
   playlistController.createPlaylist
 );
 
-playlistRouter.put("/playlist/:id", [authMiddleware.secureRoute], playlistController.getPlaylist)
+playlistRouter.put(
+  "/playlist/:id",
+  [authMiddleware.secureRoute],
+  playlistController.editPlaylist
+);
 
-playlistRouter.get("/playlist/:id", [authMiddleware.secureRoute], playlistController.getPlaylistById)
+playlistRouter.get(
+  "/playlist/:id",
+  [authMiddleware.secureRoute],
+  playlistController.getPlaylistById
+);
 
-playlistRouter.get("/userplaylists", [authMiddleware.secureRoute], playlistController.getAllUserPlaylists)
+playlistRouter.get(
+  "/userplaylists",
+  [authMiddleware.secureRoute],
+  playlistController.getAllUserPlaylists
+);
 
-playlistRouter.get("/allplaylists", [authMiddleware.secureRoute], playlistController.getAllPlaylists)
+playlistRouter.get(
+  "/allplaylists",
+  [authMiddleware.secureRoute],
+  playlistController.getAllPlaylists
+);
 
-playlistRouter.delete("/:id", [authMiddleware.secureRoute], playlistController.deletePlaylistById)
+playlistRouter.delete(
+  "/:id",
+  [authMiddleware.secureRoute],
+  playlistController.deletePlaylistById
+);
 
-playlistRouter.put("/add-song/:id", [authMiddleware.secureRoute], playlistController.addSongToPlaylist)
+playlistRouter.put(
+  "/add-song/:id",
+  [authMiddleware.secureRoute],
+  playlistController.addSongToPlaylist
+);
 
-playlistRouter.put("/remove-song/:id", [authMiddleware.secureRoute], playlistController.removeSongFromPlaylist)
-
+playlistRouter.put(
+  "/remove-song/:id",
+  [authMiddleware.secureRoute],
+  playlistController.removeSongFromPlaylist
+);
 
 module.exports = playlistRouter;
