52b1798dddf01e7a048a9d308f13970d4de07cd0
Walber Melo <dev4wm@gmail.com>
2022-10-24 15:03:36 +0100

    fix(model): Fix song model artist

--- models/songModel.js
+++ models/songModel.js
@@ -1,57 +1,59 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const songSchema = new mongoose.Schema({
-
+const songSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
-    artists: {
-        type: String,
+    artist: {
+      type: String,
     },
     rating: {
-        type: Number
+      type: Number,
     },
     url: {
-        type: String
+      type: String,
     },
     popularity: {
-        type: String
+      type: String,
     },
     duration: {
-        type: Number
+      type: Number,
     },
     userId: {
-        type: String,
-        ref: 'users'
+      type: String,
+      ref: "users",
     },
-    genere: {
-        type: String,
+    genre: {
+      type: String,
     },
-    likedBy: [{
-        type: String
-    }],
-     
-}, 
+    likedBy: [
+      {
+        type: String,
+      },
+    ],
+  },
   {
     versionKey: false,
   },
   {
-    timestamps: true
-  });
+    timestamps: true,
+  }
+);
 
 const validate = (song) => {
-    const schema = Joi.object({
-      name: Joi.string().min(2).max(50).required(),
-      artist: Joi.string().min(2).max(30).required(),
-      genere: Joi.string().min(2).max(30).required(),
-      url: Joi.string().required(),
-      duration: Joi.number().required()
-    });
-    return schema.validate(song);
-  };
+  const schema = Joi.object({
+    name: Joi.string().min(2).max(50).required(),
+    artist: Joi.string().min(2).max(30).required(),
+    genre: Joi.string().min(2).max(30).required(),
+    url: Joi.string().required(),
+    duration: Joi.number().required(),
+  });
+  return schema.validate(song);
+};
 
 const Song = mongoose.model("songs", songSchema);
 
-module.exports = {Song, validate}
+module.exports = { Song, validate };
